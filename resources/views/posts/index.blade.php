8d0426c00f255028c9cd43316c3e871e65123304
nakano1122 <145189549+nakano1122@users.noreply.github.com>
2023-10-09 00:40:19 +0900

    Revert "カリキュラム08-4終了"

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -18,7 +18,6 @@
                 <p class='body'>{{ $post->body }}</p>
             </div>
             @endforeach
-            <a href='/posts/create'>create</a>
         </div>
         <div class='paginate'>
             {{ $posts->links() }}
